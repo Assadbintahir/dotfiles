e6e1605f6c75023e0e5fc2f3d822ddd65378d4a2
Asad Ullah <asad.u@outlook.com>
2024-11-28 01:53:14 +0500

    Update scaffold_pop_os.sh

--- scaffold_pop_os.sh
+++ scaffold_pop_os.sh
@@ -7,8 +7,9 @@ sudo apt install -y openvpn tmux zsh network-manager-openvpn neovim ripgrep xcli
 sudo apt install -y gnome-shell-extensions gtk2-engines-murrine gnome-tweaks fonts-firacode
 sudo apt install -y curl git mercurial make binutils bison gcc build-essential
 
-flatpak install flathub com.bitwarden.desktop flathub md.obsidian.Obsidian flathub io.dbeaver.DBeaverCommunity flathub com.visualstudio.code flathub com.microsoft.Edge
+flatpak install flathub com.bitwarden.desktop flathub md.obsidian.Obsidian flathub io.dbeaver.DBeaverCommunity flathub com.visualstudio.code flathub com.microsoft.Edge 
 flatpak install flathub com.spotify.Client org.clementine_player.Clementine org.videolan.VLC org.audacityteam.Audacity org.gnucash.GnuCash com.obsproject.Studio com.mattjakeman.ExtensionManager org.cryptomator.Cryptomator
+flatpak install flathub com.protonvpn.www dev.fredol.open-tv 
 
 # Download and install xdman manually
 
