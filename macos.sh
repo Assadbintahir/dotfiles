24d18508da15f64b7dfdcb8aec7aa8877e0c93ec
Asad Ullah <asad.u@outlook.com>
2025-11-28 04:30:16 +0500

    Add macos.sh for SSH and Homebrew setup

---
+++ macos.sh
@@ -0,0 +1,11 @@
+# in .zshrc
+ssh-add  ~/.ssh/id_ed25519_p
+ssh-add  ~/.ssh/id_ed25519_e
+
+# brew commands
+brew install rclone
+brew install --cask kapitainsky-rclone-browser
+brew install bitwarden 
+brew install --cask vlc
+brew install wget
+brew install tmux
