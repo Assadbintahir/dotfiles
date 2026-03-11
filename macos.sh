7b22c188fc2960c9694b8d92dfa7ea210f25efb6
Asad Ullah <asad.u@outlook.com>
2026-03-11 22:32:08 +0500

    Add installation of Helix to macos.sh

--- macos.sh
+++ macos.sh
@@ -16,5 +16,6 @@ brew install --cask alt-tab
 brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
 brew install maven
+brew install helix
 
 # office: https://github.com/alsyundawy/Microsoft-Office-For-MacOS
